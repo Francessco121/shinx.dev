86a0de4e11addaa1bb5b2197756042bd5e42ac3f
Ethan Lafrenais <elafrenais12@gmail.com>
2017-06-08 22:22:52 -0400

    Add initial site

---
+++ README.md
@@ -0,0 +1,3 @@
+# francessco121.github.io
+
+Personal website generated from a personal tool.
---
+++ js/components/fullscreen-images.js
@@ -0,0 +1,19 @@
+var fullscreenImages = {
+    showImage: function(url) {
+		$('.fullscreen-image').attr('src', url);
+		$('.fullscreen-image-wrapper').show();
+	},
+	
+	hideImage: function() {
+		$('.fullscreen-image-wrapper').hide();
+	}
+}
+
+$(document).ready(function () {
+    $('.fullscreen-image-wrapper').on('click', function() {
+        fullscreenImages.hideImage();
+    });
+    $('.fullscreen-image').on('click', function(evt) {
+        evt.stopPropagation();
+    });
+});
