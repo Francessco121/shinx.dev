ca70806634d6a148de56a88036951e0ffea67d20
Ethan Lafrenais <elafrenais12@gmail.com>
2018-10-11 18:48:19 -0400

    Finish publish script

--- tool/publish.bat
+++ tool/publish.bat
@@ -1,31 +1,44 @@
 @echo off
+setlocal
+
+rem Enter project root
+cd ..
 
 rem Build the project
-pub run build_runner build -o build -r
+echo Building...
+call pub run build_runner build -o build -r
 
 rem Save the dev commit hash for later
-for /F "delims=" %i in ('git rev-parse HEAD') do set commitHash=%i
+for /F "delims=" %%i in ('git rev-parse HEAD') do set commitHash=%%i
 
 rem Enter build web directory
 cd build\web
 
 rem Delete unnecessary packages sym link
+echo Deleting packages sym link...
 rmdir packages
 
 rem Initialize a git repo
-git init
-git remote add origin https://github.com/Francessco121/francessco121.github.io.git
-git fetch
+echo Initializing git repository...
+call git init
+call git remote add origin https://github.com/Francessco121/francessco121.github.io.git
+call git fetch
 
 rem Reset to master
-git reset --mixed origin/master
+echo Resetting to master...
+call git reset --mixed origin/master
 
 rem Commit changes
-git add .
-git commit -m "Deploy from dev/%variable%"
+echo Commiting...
+call git add .
+call git commit -m "Deploy from dev/%commitHash%"
 
 rem Push
-git push
+echo "Pushing..."
+call git push --set-upstream origin master
 
 rem All done!
+echo Successfully deployed!
 pause
+
+endlocal
