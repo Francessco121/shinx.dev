4f7051589e9e4bcaecda06eca547fe9d2be07f92
Ethan Lafrenais <elafrenais12@gmail.com>
2018-10-12 22:29:48 -0400

    Re-write most of the website, start using Sass, update data/info.

--- web/js/components/header-nav.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    function toggleHeaderNav() {
-        $('.header-nav-mobile-pane').toggleClass('active');
-        $('.header-nav-mobile-pane-cancel').toggleClass('active');
-    }
-
-    $('[data-toggle="header-nav"]').click(toggleHeaderNav);
-    $('.header-nav-mobile-pane-cancel').click(toggleHeaderNav);
-});
